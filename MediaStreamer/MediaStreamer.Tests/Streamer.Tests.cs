0425f3a6573e52d0c92034a33ddcdcb06cbf7ae0
Yuriy Musiychuk <ymusiychuk@lohika.com>
2016-09-26 12:49:29 +0300

    Test CreatePipelineIsCalledOnStartStreamForFile added

--- MediaStreamer/MediaStreamer.Tests/Streamer.Tests.cs
+++ MediaStreamer/MediaStreamer.Tests/Streamer.Tests.cs
@@ -23,5 +23,23 @@ namespace MediaStreamer.Tests
 
             _mediaDeviceWrapperMock.Verify(m => m.Init(It.Is<IntPtr>(i => i.ToInt32() == 12345)));
         }
+
+        [Test]
+        public void CreatePipelineIsCalledOnStartStreamForFile()
+        {
+            Streamer streamer = new Streamer(_mediaDeviceWrapperMock.Object, new IntPtr(12345));
+
+            int width, height;
+            IntPtr surface;
+            RenderCallback callback = (int x, int y) => { };
+            streamer.StartStream("foo", out width, out height, out surface, callback);
+
+            _mediaDeviceWrapperMock.Verify(m => m.CreatePipeline(
+                It.Is<string>(s => s == "foo"),
+                out surface,
+                ref width,
+                ref height,
+                It.Is<RenderCallback>(c => c == callback)));
+        }
     }
 }
